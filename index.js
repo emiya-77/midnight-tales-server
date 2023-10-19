e5c51731abdd21e54e249eb3326894b8b037d96f
Golam Kibria <uchihai850@gmail.com>
2023-10-20 02:39:08 +0600

    GET, POST:
    This commit implements the 'GET' api that is being used in the client to access data for the 'My-Cart' page. This commit also implements the 'POST' api, that is being used for the 'Add Product' page. 'Add Product' page reads the input from the user and calls the POST api in the backend to upload the data into the MongoDB's database, named showsCollection.

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
-
+// middleware
 app.use(cors());
 app.use(express.json());
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.phi4gnz.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -21,20 +20,103 @@ const client = new MongoClient(uri, {
   }
 });
 
+
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        
+        const showsCollection = client.db('showsDB').collection('show');
+        const userShowsCollection = client.db('showsDB').collection('userShows');
+
+        app.get('/show', async (req, res) => {
+            const cursor = showsCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+        app.get('/show/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)};
+            const result = await showsCollection.findOne(query);
+            res.send(result);
+        })
+
+        app.get('/user-shows/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = {userId: id};
+            const result = await userShowsCollection.findOne(query);
+            res.send(result);
+        })
+
+        app.post('/show', async (req, res) => {
+            const newShow = req.body;
+            console.log(newShow);
+            const result = await showsCollection.insertOne(newShow);
+            res.send(result);
+        })
+
+        app.put('/show/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = {_id: new ObjectId(id)};
+            const options = {upsert: true};
+            const updatedShow = req.body;
+            const show = {
+                $set: {
+                    image: updatedShow.name,
+                    name: updatedShow.quantity,
+                    brandName: updatedShow.supplier,
+                    type: updatedShow.taste,
+                    price: updatedShow.category,
+                    rating: updatedShow.details
+                }
+            }
+
+            const result = await showsCollection.updateOne(filter, show, options);
+            res.send(result);
+        })
+
+        app.put('/user-shows/:id', async (req, res) => {
+            const id = req.params.id;
+            const updatedUserShows = req.body;
+            
+            const filter = {userId: id};
+            const existingUser = await userShowsCollection.findOne(filter);
+
+            console.log(existingUser);
+
+            if(existingUser){
+                const options = {upsert: true};
+                const userCart = {
+                    $set: {
+                        userId: updatedUserShows.userId,
+                        updatedUserCartList: updatedUserShows.updatedUserCartList
+                    }
+                }
+                const updateResult = await userShowsCollection.updateOne(filter, userCart, options);
+                res.send(updateResult);
+            }else{
+                const insertResult = await userShowsCollection.insertOne(updatedUserShows);
+                res.send(insertResult);
+            }
+        })
+
+        // app.delete('/movie/:id', async (req, res) => {
+        //     const id = req.params.id;
+        //     const query = {_id: new ObjectId(id)};
+        //     const result = await showsCollection.deleteOne(query);
+        //     res.send(result);
+        // })
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
 }
 run().catch(console.dir);
-
 
 
 
